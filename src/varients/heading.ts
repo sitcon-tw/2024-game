a2bb079e2bc97d3f713ea0f2745db35d39257591
SimbaFs <simba.fs@gmail.com>
2024-02-02 21:15:28 +0800

    varients

--- src/varients/heading.ts
+++ src/varients/heading.ts
@@ -1,7 +1,18 @@
 import { tv } from "tailwind-variants";
 
 export const heading = tv({
-  base: "",
-  variants: {},
-  defaultVariants: {},
+  base: "font-sans font-bold",
+  variants: {
+    level: {
+      1: 'text-[30px] leading-[36px]',
+      2: 'text-[24px] leading-[28.8px]',
+      3: 'text-[22px] leading-[26.4px]',
+      4: 'text-[20px] leading-[24px]',
+      5: 'text-[18px] leading-[21.6px]',
+    }
+  },
+  defaultVariants: {
+    level: 1,
+  },
 });
+
--- src/varients/text.tsx
+++ src/varients/text.tsx
@@ -1,7 +1,21 @@
 import { tv } from "tailwind-variants";
 
 export const text = tv({
-  base: "",
-  variants: {},
-  defaultVariants: {},
+  base: "font-sans",
+  variants: {
+    level: {
+      1: 'text-lg',
+      2: 'text-base',
+      3: 'text-sm',
+      4: 'text-xs leading-[18px]',
+    },
+    bold: {
+      true: 'font-medium',
+      false: 'font-normal',
+    }
+  },
+  defaultVariants: {
+    level: 1,
+    bold: false,
+  },
 });
