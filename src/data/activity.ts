42eef0d70c3ef0de3304b429142573e806bf1853
SimbaFs <simba.fs@gmail.com>
2024-02-03 10:18:11 +0800

    update the format of data

--- src/data/activity.ts
+++ src/data/activity.ts
@@ -1,7 +1,7 @@
 export type ActivityType = {
     name: string;
     description?: string;
-    links?: string[];
+    link?: string;
 };
 
 // 議程
@@ -9,108 +9,110 @@ const sessions: ActivityType[] = [
     {
         name: "議程一",
         description: "議程一的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "議程二",
         description: "議程二的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "議程三",
         description: "議程三的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "議程四",
         description: "議程四的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "議程五",
         description: "議程五的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "議程六",
         description: "議程六的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "議程七",
         description: "議程七的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "議程八",
         description: "議程八的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "議程九",
         description: "議程九的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "議程十",
         description: "議程十的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
 ];
 
+// 攤位
 const booths: ActivityType[] = [
     {
         name: "攤位一",
         description: "攤位一的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "攤位二",
         description: "攤位二的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "攤位三",
         description: "攤位三的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "攤位四",
         description: "攤位四的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "攤位五",
         description: "攤位五的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "攤位六",
         description: "攤位六的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "攤位七",
         description: "攤位七的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "攤位八",
         description: "攤位八的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "攤位九",
         description: "攤位九的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
     {
         name: "攤位十",
         description: "攤位十的描述",
-        links: ["https://sitcon.org/2024"],
+        link: "https://sitcon.org/2024",
     },
 ];
 
+// 特殊活動
 const others: ActivityType[] = [
     {
         name: "特殊活動一",
