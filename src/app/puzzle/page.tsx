c0ca5cb5f4ba0923b8183019c99aaeeb7a2487be
YuDong <yuchi9716@gmail.com>
2024-02-13 02:48:55 +0800

    just add the menu in puzzle page

--- src/app/puzzle/page.tsx
+++ src/app/puzzle/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <div>Puzzle</div>;
+'use client';
+import { Menu } from '@/components/Menu'
+import React, { useState } from 'react'
+
+const page = () => {
+  const [active, setActive] = useState(0);
+  return (
+    <div>
+      <Menu
+        texts={["拼圖1", "拼圖2", "拼圖3"]}
+        active={active}
+        setActive={setActive}
+      />
+    </div>
+  )
 }
+
+export default page
