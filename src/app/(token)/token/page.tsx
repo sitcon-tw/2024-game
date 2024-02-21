a615afb9d2b07a51275fb7d3d5429f8cfe675a00
gnehs <jayuiop@gmail.com>
2024-02-21 22:14:42 +0800

    warp page

--- src/app/(token)/token/page.tsx
+++ src/app/(token)/token/page.tsx
@@ -22,39 +22,41 @@ export default function Page() {
     }, 500);
   }
   return (
-    <motion.div
-      className="mx-auto w-full max-w-[512px] px-2 py-10"
-      initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 100 }}
-      transition={{ duration: 0.5 }}
-    >
-      <h1 className="text-center text-4xl font-bold">
-        你發現了拼圖碎片
-        <span className="absolute">！</span>
-      </h1>
-      <div className="my-4 flex flex-wrap items-center justify-between gap-6 rounded-2xl bg-gray-50 p-4 pl-6 text-center text-6xl shadow">
-        <div
-          className="grow overflow-hidden text-ellipsis p-2 text-left"
-          style={{
-            fontFamily: `"Roboto Mono", monospace`,
-          }}
-        >
-          {token}
+    <div>
+      <motion.div
+        className="mx-auto w-full max-w-[512px] px-2 py-10"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 100 }}
+        transition={{ duration: 0.5 }}
+      >
+        <h1 className="text-center text-4xl font-bold">
+          你發現了拼圖碎片
+          <span className="absolute">！</span>
+        </h1>
+        <div className="my-4 flex flex-wrap items-center justify-between gap-6 rounded-2xl bg-gray-50 p-4 pl-6 text-center text-6xl shadow">
+          <div
+            className="grow overflow-hidden text-ellipsis p-2 text-left"
+            style={{
+              fontFamily: `"Roboto Mono", monospace`,
+            }}
+          >
+            {token}
+          </div>
+          <motion.button
+            onClick={copy}
+            className="mx-auto overflow-hidden rounded-2xl bg-purple-500 px-6 py-2 text-2xl font-bold text-white shadow-md"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            key={copied ? "copied" : "copy"}
+          >
+            {copied ? "已複製" : "複製"}
+          </motion.button>
         </div>
-        <motion.button
-          onClick={copy}
-          className="mx-auto overflow-hidden rounded-2xl bg-purple-500 px-6 py-2 text-2xl font-bold text-white shadow-md"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          key={copied ? "copied" : "copy"}
-        >
-          {copied ? "已複製" : "複製"}
-        </motion.button>
-      </div>
-      <div className="text-center text-2xl text-gray-600">
-        在 OPass &gt; 大地遊戲 &gt; 掃描器中輸入以下代碼來取得拼圖
-      </div>
-    </motion.div>
+        <div className="text-center text-2xl text-gray-600">
+          在 OPass &gt; 大地遊戲 &gt; 掃描器中輸入以下代碼來取得拼圖
+        </div>
+      </motion.div>
+    </div>
   );
 }
