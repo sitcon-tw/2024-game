ef76152f46097017e6aa12f92aef3bb7bf61f624
YuDong <yuchi9716@gmail.com>
2024-02-23 01:17:49 +0800

    try fix

--- src/components/Scanner.tsx
+++ src/components/Scanner.tsx
@@ -2,18 +2,15 @@
 import React, { useState } from 'react';
 import { QrReader } from 'react-qr-reader';
 import {InfoWindow} from '../components/InfoWindow';
-import { sendPuzzle2Player } from '@/lib/sendPuzzle2Player';
-import { getPlayerPuzzle } from '@/lib/getPlayerPuzzle';
 
 const Scanner = () => {
   const [IsgetData, setIsgetData] = useState(false);
-  const [startScan, setStartScan] = useState(true);
   const [facingMode, setFacingMode] = useState('environment');
 
   return (
-    <div className='flex'>
-      <div className='w-full'>        
-        <div className='justify-center text-center items-center mt-24'>
+    <div className='flex justify-center text-center items-center'>
+      <div className='w-full'>    
+        <div className='mt-24'>
             <select onChange={(e) => setFacingMode(e.target.value)}>
               <option value={"environment"}>後置鏡頭</option>
               <option value={"user"}>前置鏡頭</option>
