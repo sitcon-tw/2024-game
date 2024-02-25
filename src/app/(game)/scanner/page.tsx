1b184480f332cea70bb1b6279ed87f8c9f822611
gnehs <jayuiop@gmail.com>
2024-02-25 21:37:55 +0800

    refactor scanner

--- src/app/(game)/scanner/page.tsx
+++ src/app/(game)/scanner/page.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import Scanner from "@/components/Scanner";
 
+import { InfoWindow } from "@/components/InfoWindow";
 const page = () => {
-  return <Scanner />;
+  const [result, setResult] = useState<string | null>(null);
+
+  return (
+    <>
+      <Scanner
+        onResult={(result) => {
+          setResult(result);
+        }}
+      />
+
+      {result && (
+        <InfoWindow msg="恭喜獲得一塊拼圖!!" onClose={() => setResult(null)} />
+      )}
+    </>
+  );
 };
 
 export default page;
--- src/components/Scanner.tsx
+++ src/components/Scanner.tsx
@@ -1,9 +1,14 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { QrReader } from "react-qr-reader";
-import { InfoWindow } from "../components/InfoWindow";
-
-function Scanner() {
+// Override console.error
+// This is a hack to suppress the warning about missing defaultProps in react-qr-reader
+const error = console.error;
+console.error = (...args: any) => {
+  if (/defaultProps/.test(args[0])) return;
+  error(...args);
+};
+function Scanner({ onResult }: { onResult: (result: string) => void }) {
   useEffect(() => {
     getUserCameraList();
   }, []);
@@ -14,63 +19,51 @@ function Scanner() {
     setCameras(devices);
     setCurrentCamera(devices[0].deviceId);
   }
-  const [IsgetData, setIsgetData] = useState(false);
   const [cameras, setCameras] = useState<MediaDeviceInfo[]>([]);
   const [currentCamera, setCurrentCamera] = useState<string | null>(null);
 
   return (
     <div className="relative flex h-full w-full items-center justify-center text-center">
-      <div className="h-full w-full">
-        <div>
-          <select
-            onChange={(e) => setCurrentCamera(e.target.value)}
-            value={currentCamera || ""}
-            className="absolute left-0 right-0 top-4 z-10 m-auto w-[256px] rounded-md border border-gray-300 bg-white p-2 shadow-md"
-          >
-            {cameras.map((camera) => (
-              <option key={camera.deviceId} value={camera.deviceId}>
-                {camera.label}
-              </option>
-            ))}
-          </select>
-        </div>
+      <select
+        onChange={(e) => setCurrentCamera(e.target.value)}
+        value={currentCamera || ""}
+        className="absolute left-0 right-0 top-4 z-10 m-auto w-[256px] rounded-md border border-gray-300 bg-white p-2 shadow-md"
+      >
+        {cameras.map((camera) => (
+          <option key={camera.deviceId} value={camera.deviceId}>
+            {camera.label}
+          </option>
+        ))}
+      </select>
 
-        {currentCamera && (
-          <QrReader
-            key={currentCamera}
-            onResult={(result) => {
-              if (!!result) {
-                // alert(data);
-                // check the data if it is valid
-                setIsgetData(true);
-                // sendPuzzle2Player('7679f08f7eaeef5e9a65a1738ae2840e', '') send Puzzle to the player, how to get playerToken
-              }
-            }}
-            constraints={{
-              deviceId: currentCamera!,
-            }}
-            videoStyle={{
-              objectFit: "cover",
-              height: "100%",
-              width: "100%",
-              position: "relative",
-            }}
-            videoContainerStyle={{
-              height: "100%",
-              width: "100%",
-              padding: "0",
-            }}
-            containerStyle={{
-              height: "100%",
-              width: "100%",
-            }}
-          />
-        )}
-      </div>
-      {IsgetData && (
-        <InfoWindow
-          msg="恭喜獲得一塊拼圖!!"
-          onClose={() => setIsgetData(false)}
+      {currentCamera && (
+        <QrReader
+          key={currentCamera}
+          onResult={(result) => {
+            const text = result?.getText();
+            if (text) {
+              console.log(text);
+              onResult(text);
+            }
+          }}
+          constraints={{
+            deviceId: currentCamera!,
+          }}
+          videoStyle={{
+            objectFit: "cover",
+            height: "100%",
+            width: "100%",
+            position: "relative",
+          }}
+          videoContainerStyle={{
+            height: "100%",
+            width: "100%",
+            padding: "0",
+          }}
+          containerStyle={{
+            height: "100%",
+            width: "100%",
+          }}
         />
       )}
     </div>
