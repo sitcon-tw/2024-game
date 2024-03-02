b0de1f922e423a9f392db6ba837fc47bb04d2280
gnehs <jayuiop@gmail.com>
2024-03-02 20:21:10 +0800

    refactor footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,52 +1,48 @@
 "use client";
 
-import Image from "next/image";
+import { Puzzle, ScanLine, ListTodo } from "lucide-react";
 import Link from "next/link";
 import { twMerge } from "tailwind-merge";
 import { usePathname } from "next/navigation";
 import { text } from "@/varients/text";
 
-import home from "@/img/home.svg";
-import scanner from "@/img/scanner.svg";
-import puzzle from "@/img/puzzle.svg";
-
 function NavLink({
   href,
-  img,
-  title,
+  children,
 }: {
   href: string;
-  img: any;
-  title: string;
+  children: React.ReactNode;
 }) {
   const pathname = usePathname();
-
   return (
     <Link
       href={href}
       className={twMerge(
         "flex h-16 flex-col items-center justify-between",
+        text({ level: 2, bold: true }),
         pathname !== href && "opacity-60",
       )}
     >
-      <Image src={img} width={40} height={40} alt="" />
-      <p className={text({ level: 2, bold: true })}>{title}</p>
+      {children}
     </Link>
   );
 }
 
-const links = [
-  { href: "/", img: home, title: "任務" },
-  { href: "/scanner", img: scanner, title: "掃描器" },
-  { href: "/puzzle", img: puzzle, title: "我的拼圖" },
-] as const;
-
 export function Footer() {
   return (
     <footer className="grid grid-cols-3 bg-sitcon-primary py-2 text-sitcon-secondary">
-      {links.map((link) => (
-        <NavLink key={link.href} {...link} />
-      ))}
+      <NavLink href="/">
+        <ListTodo size={36} strokeWidth={1.75} />
+        任務
+      </NavLink>
+      <NavLink href="/scanner">
+        <ScanLine size={36} strokeWidth={1.75} />
+        掃描器
+      </NavLink>
+      <NavLink href="/puzzle">
+        <Puzzle size={36} strokeWidth={1.75} />
+        我的拼圖
+      </NavLink>
     </footer>
   );
 }
