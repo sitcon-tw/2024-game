c71294b30b6e954fb54d179c8d2e26a1cad91131
Vincent550102 <5020559@gmail.com>
2024-03-06 20:51:36 +0800

    update scanner warning

--- src/components/Scanner.tsx
+++ src/components/Scanner.tsx
@@ -50,27 +50,28 @@ function Scanner({ onResult }: { onResult: (result: string) => void }) {
   const [currentCamera, setCurrentCamera] = useState<string>(manual);
 
   return (
-    <div className="relative flex h-full min-h-[396px] w-full items-center justify-center text-center">
-      <Select
-        onChange={(e) => setCurrentCamera(e.target.value)}
-        value={currentCamera || ""}
-        className="w-full appearance-none rounded-md bg-white/75 p-2 shadow-md outline-0 backdrop-blur-lg"
-        containerClassName="absolute left-0 right-0 top-4 z-[1] w-[256px] m-auto"
-      >
-        <option value={manual}>手動輸入</option>
-        {cameras.map((camera) => (
-          <option key={camera.deviceId} value={camera.deviceId}>
-            {camera.label}
-          </option>
-        ))}
-      </Select>
+    <>
+      <p>若遇到掃瞄器出不來的情況，多切換幾次或使用手動輸入</p>
+      <div className="relative flex h-full min-h-[396px] w-full items-center justify-center text-center">
+        <Select
+          onChange={(e) => setCurrentCamera(e.target.value)}
+          value={currentCamera || ""}
+          className="w-full appearance-none rounded-md bg-white/75 p-2 shadow-md outline-0 backdrop-blur-lg"
+          containerClassName="absolute left-0 right-0 top-4 z-[1] w-[256px] m-auto"
+        >
+          <option value={manual}>手動輸入</option>
+          {cameras.map((camera) => (
+            <option key={camera.deviceId} value={camera.deviceId}>
+              {camera.label}
+            </option>
+          ))}
+        </Select>
 
-      {currentCamera === "manual" ? (
-        <Manual onResult={onResult} />
-      ) : (
-        <FromCamera currentCamera={currentCamera} onResult={onResult} />
-      )}
-    </div>
+        {currentCamera === "manual"
+          ? <Manual onResult={onResult} />
+          : <FromCamera currentCamera={currentCamera} onResult={onResult} />}
+      </div>
+    </>
   );
 }
 
